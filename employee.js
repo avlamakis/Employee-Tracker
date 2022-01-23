5b5b343712f662e72ff3ea92d1d9dc6eee0ad27b
alexvlamakis <alexvlamakis@gmail.com>
2022-01-22 19:10:44 -0700

    view all employees function and view all departments function created

--- employee.js
+++ employee.js
@@ -1,8 +1,9 @@
 // List the dependencies here.
 const Sequelize = require('sequelize');
 const inquirer = require('inquirer');
-const consoleTable = require('console.table');
 const util = require('util');
+
+require('console.table');
 
 const promptMessages = {
     viewAllEmployees: "View All Employees",
@@ -78,8 +79,43 @@ function prompt() {
                     break;
 
                 case promptMessages.exit:
-                    connection.end();
+                    sequelize.end();
                     break;
             }
         });
 }
+
+function viewAllEmployees() {
+    const query = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+    FROM employee
+    LEFT JOIN employee manager on manager.id = employee.manager_id
+    INNER JOIN role ON (role.id = employee.role_id)
+    INNER JOIN department ON (department.id = role.department_id)
+    ORDER BY employee.id;`;
+    sequelize.query(query, (err, res) => {
+        if (err) throw err;
+        console.log('\n');
+        console.log('VIEW ALL EMPLOYEES');
+        console.log('\n');
+        console.table(res);
+        prompt();
+    });
+}
+
+function viewByDepartment() {
+    const query = `SELECT department.name AS department, role.title, employee.id, employee.first_name, employee.last_name
+    FROM employee
+    LEFT JOIN role ON (role.id = employee.role_id)
+    LEFT JOIN department ON (department.id = role.department_id)
+    ORDER BY department.name;`;
+    sequelize.query(query, (err, res) => {
+        if (err) throw err;
+        console.log('\n');
+        console.log('VIEW EMPLOYEE BY DEPARTMENT');
+        console.log('\n');
+        console.table(res);
+        prompt();
+    });
+}
+
+
