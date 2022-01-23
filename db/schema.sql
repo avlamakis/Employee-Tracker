5ef287cff3ea5cf802c31ec13ea0bfc1058ccbeb
alexvlamakis <alexvlamakis@gmail.com>
2022-01-22 17:39:23 -0700

    tables created in schema file and added gitignore

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db
+
+USE employees_db;
+
+CREATE TABLE department (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL UNSIGNED NOT NULL,
+    department_id INT UNSIGNED NOT NULL,
+);
+
+CREATE TABLE employee (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR (30) NOT NULL,
+    role_id INT UNSIGNED NOT NULL,
+    manager_id INT UNSIGNED
+);
+
+
