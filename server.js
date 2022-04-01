a87e49ced04b56ae52f17f361a2d1ccffa1a65c2
alexvlamakis <alexvlamakis@gmail.com>
2022-04-01 06:15:29 -0600

    server start, password error

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: process.env.password,
+    password: 'Alexthunder35!',
     database: 'employeeTrackerDB',
 });
 
